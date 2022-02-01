6f2db1900813a5e2d08906acbb20d45e64e349f2
Jiří Fuks <42421640+Dontjirka@users.noreply.github.com>
2022-02-01 21:44:00 +0000

    Add files via upload

---
+++ Triangle/Is_this_triangle.py
@@ -0,0 +1,8 @@
+a = float(input("Enter one side of triangle (a): "))
+b = float(input("Enter second side of triangle (b): "))
+c = float(input("Enter third side of triangle (c): "))
+
+if a + b > c and a + c > b and b + c > a:
+    print("It is triangle")
+else:
+    print("It is not a triangle")

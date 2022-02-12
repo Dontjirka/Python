e49ea0622ed29b1dc198a77c173c03362a3a554a
Jiří Fuks <42421640+Dontjirka@users.noreply.github.com>
2022-02-12 22:28:53 +0000

    Update main.py

--- Rock, paper, scissor/main.py
+++ Rock, paper, scissor/main.py
@@ -8,24 +8,42 @@ computer = str(rand.choices(game))
 #Rock
 if computer == "['Rock']":
     if player == "R":
-        print("Tie")
+        print("Robot: Rock")
+        print("You: Rock")
+        print("It's tie")
     elif player == "S":
+        print("Robot: Rock")
+        print("You: Scissor")
         print("You lose")
     else:
+        print("Robot: Rock")
+        print("You: Paper")
         print("You win")
 #Paper
 if computer == "['Paper']":
     if player == "P":
-        print("Tie")
+        print("Robot: Paper")
+        print("You: Paper")
+        print("It's tie")
     elif player == "R":
+        print("Robot: Paper")
+        print("You: Rock")
         print("You lose")
     else:
+        print("Robot: Paper")
+        print("You: Scissor")
         print("You win")
 #Scissor
 if computer == "['Scissor']":
     if player == "S":
-        print("Tie")
+        print("Robot: Scissor")
+        print("You: Scissor")
+        print("It's tie")
     elif player == "P":
+        print("Robot: Scissor")
+        print("You: Paper")
         print("You lose")
     else:
+        print("Robot: Scissor")
+        print("You: Rock")
         print("You win")

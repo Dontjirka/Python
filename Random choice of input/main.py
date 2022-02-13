caed5a16cbcefef0577c3af0c6f1acef754f2fab
Jiří Fuks <42421640+Dontjirka@users.noreply.github.com>
2022-02-13 18:23:47 +0000

    Delete Random choice of input directory

--- Random choice of input/main.py
+++
@@ -1,9 +0,0 @@
-import random
-
-print("Random choice of input")
-names = str(input("Enter names: "))
-names_split = names.split(", ")
-pay_int = random.randint(0, len(names_split))
-pay = names_split[pay_int - 1]
-
-print(pay)

d437c9016c4a04199adb05daddb24174a31620e8
Jiří Fuks <42421640+Dontjirka@users.noreply.github.com>
2022-03-30 15:33:54 +0000

    Add files via upload

---
+++ countdown_timer/main.py
@@ -0,0 +1,27 @@
+import time
+from os import system, name
+
+def clear():
+   # for windows
+   if name == 'nt':
+        _ = system('cls')
+
+   # for mac and linux
+   else:
+        _ = system('clear')
+
+def countdown():
+    Time = int(input("Enter: "))
+    timing = True
+    while timing is not False:
+        for i in range(Time+1):
+            print(f"Remaining time is {Time}")
+            Time -= 1
+            time.sleep(1)
+            clear()
+            if Time == 0:
+                timing = False
+    print("Ran out of time")
+
+clear()
+countdown()

a43f95713872f5e0c072650c437149de5ccabf5b
Jiří Fuks <42421640+Dontjirka@users.noreply.github.com>
2023-01-28 08:20:20 +0100

    Add files via upload

---
+++ LIFO_FIFO/main.py
@@ -0,0 +1,31 @@
+#jelikoz python nema interfaces tak pouzivam misto toho Absctract Base Class (ABC)
+from abc import ABC, abstractmethod
+
+class Pushable(ABC):
+    @abstractmethod
+    def push(self, value):
+        pass
+
+    @abstractmethod
+    def pop(self):
+        pass
+
+class LIFO(Pushable):
+    def __init__(self):
+        self.stack = []
+
+    def push(self, value):
+        self.stack.append(value)
+
+    def pop(self):
+        return self.stack.pop()
+
+class FIFO(Pushable):
+    def __init__(self):
+        self.queue = []
+
+    def push(self, value):
+        self.queue.append(value)
+
+    def pop(self):
+        return self.queue.pop(0)
